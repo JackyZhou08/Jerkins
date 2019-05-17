659907f7775411e6c3f9a3f05b954a40b9dffe11
Jacky <jackyzhou@ideaculture.com>
2019-05-17 16:34:07 +0800

    cehis

--- app/src/main/java/com/jacky/jerkins/MainActivity.java
+++ app/src/main/java/com/jacky/jerkins/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv_url = findViewById(R.id.tv_url);
-        tv_url.setText(BuildConfig.FLAVOR + BuildConfig.BUILD_TYPE + "master");
+        tv_url.setText(BuildConfig.FLAVOR + BuildConfig.BUILD_TYPE + "develop");
     }
 }
